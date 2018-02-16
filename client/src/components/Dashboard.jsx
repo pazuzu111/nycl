8709d4ec712c2f68ab12ed19d85fc2f49a45ce1f
pazuzu111 <pazuzublue111@gmail.com>
2018-02-15 21:40:46 -0500

    created user dashboard component

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,11 +1,11 @@
-// import React from 'react'
+import React from 'react'
 
-// const Dashboard = (props) => {
-//     return (
-//         <div>
-//             <h1>Hello {props.user.username}</h1>
-//         </div>
-//     )
-// }
+const Dashboard = (props) => {
+    return (
+        <div>
+            <h1>Hello {props.user.username}</h1>
+        </div>
+    )
+}
 
-// export default Dashboard
+export default Dashboard
