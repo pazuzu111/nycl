d0e3f15b482e48a92aace453b73f1ff874abb779
pazuzu111 <pazuzublue111@gmail.com>
2018-02-15 21:17:03 -0500

    ran migration and created client

--- db/migrations/migration-11162017.sql
+++ db/migrations/migration-11162017.sql
@@ -1,8 +1,8 @@
--- DROP TABLE IF EXISTS users;
--- CREATE TABLE IF NOT EXISTS users (
---   id SERIAL PRIMARY KEY,
---   username VARCHAR(255) UNIQUE NOT NULL,
---   password_digest TEXT NOT NULL,
---   email VARCHAR(255),
---   block BOOLEAN DEFAULT FALSE
--- );
+DROP TABLE IF EXISTS users;
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(255) UNIQUE NOT NULL,
+  password_digest TEXT NOT NULL,
+  email VARCHAR(255),
+  block BOOLEAN DEFAULT FALSE
+);
